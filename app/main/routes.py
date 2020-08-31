0a733da7631876328e94ef2de3a16233b4e6a463
ilkut <ilkutt@gmail.com>
2020-08-31 11:55:16 +0100

    ready for public

--- app/main/routes.py
+++ app/main/routes.py
@@ -12,14 +12,15 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
-    # g.locale = str(get_locale())
 
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    return render_template('index.html', title=('Home'))
+    posts = Post.query.all()
+    printThis = 'string'
+    return render_template('index.html', title=('Home'), posts = posts)
 
 
 @bp.route('/user/<username>')
